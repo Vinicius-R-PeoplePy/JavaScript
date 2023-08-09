a3e3967860264401596bd9767b28271905cca15a
Vinícius.People.Py <109100850+Vinicius-R-PeoplePy@users.noreply.github.com>
2023-08-09 17:30:04 -0300

    Create operações-básicas.js

---
+++ operações-básicas.js
@@ -0,0 +1,49 @@
+//var primeiroNumero = 20;
+//var segundoNumero = 7;
+
+//console.log('Soma dos dois números = ', primeiroNumero + segundoNumero)
+
+
+// operações matemáticas 
+
+var resultado; 
+var x = 2;
+var y = 5;
+
+
+// soma 
+resultado = x + y; 
+
+console.log('Soma:', resultado)
+
+// subtração 
+
+console.log('Subtração: ', resultado)
+
+// multiplicação 
+resultado = x * y 
+
+console.log('Multiplicação:', resultado)
+
+// divisão
+resultado = x / y; 
+console.log('Divisão', resultado)
+
+// cálculo do módulo (resto da divisão)
+
+resultado = x % y;
+console.log('IMC= ', resultado)
+
+// cálculo avançado 
+var peso = 80 
+var altura = 1.75 
+
+var imc = (peso / (altura*altura))
+console.log('IMC= ', imc)
+
+// soma de strings 
+var primeiroNome = 'Gabriel'
+var segundoNome = 'Rangel'
+
+console.log('Nome Completo -> ', primeiroNome + ' ' + segundoNome)
+
